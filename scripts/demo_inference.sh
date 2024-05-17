0a1e98f4e35d095d51ee2786b77b36d5e4025fe9
zengjianping <happysjfzjp@126.com>
2024-05-17 21:22:32 +0800

    dev

--- scripts/demo_inference.sh
+++ scripts/demo_inference.sh
@@ -2,8 +2,8 @@ set -x
 
 export PYTHONPATH=.:$PYTHONPATH
 
-CONFIG=configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml
-CKPT=pretrained_models/fast_res50_256x192.pth
+#CONFIG=configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml
+#CKPT=pretrained_models/fast_res50_256x192.pth
 
 CONFIG=configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-dcn-combined.yaml
 CKPT=pretrained_models/multi_domain_fast50_dcn_combined_256x192.pth
@@ -11,21 +11,21 @@ CKPT=pretrained_models/multi_domain_fast50_dcn_combined_256x192.pth
 #CONFIG=configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml
 #CKPT=pretrained_models/multi_domain_fast50_regression_256x192.pth
 
-VIDEO=datas/golf_videos/test01.mp4
 INDIR=examples/demo
 OUTDIR=examples/res
 
-INDIR=/data/ModelTrainData
-IMGLIST=datas/golf_annotations/task01/image_list.txt
+#VIDEO=datas/golf_videos/test01.mp4
+#INDIR=/data/ModelTrainData
+#IMGLIST=datas/golf_annotations/task01/image_list.txt
 
 python scripts/demo_inference.py \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --detector yolo \
+    --save_img \
     --indir ${INDIR} \
-    --list ${IMGLIST} \
     --outdir ${OUTDIR}
 
-#    --save_img \
+#    --list ${IMGLIST} \
 #    --video ${VIDEO} \
 #    --save_video \
